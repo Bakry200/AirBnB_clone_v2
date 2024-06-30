859b8e74b7119ab2c68661013aea93d253ffb810
Bakry200 <bakryalfadul@gmail.com>
2024-06-29 17:22:11 -0700

    sort

---
+++ setup_mysql_test.sql
@@ -0,0 +1,5 @@
+-- Create test database and user
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT USAGE ON *.* TO 'hbnb_test'@'localhost';
+GRANT ALL ON hbnb_test_db.* TO 'hbnb_test'@'localhost';

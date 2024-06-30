1f3713318976495831082a1cc8f578f795eed88c
Bakry200 <bakryalfadul@gmail.com>
2024-06-29 17:42:34 -0700

    pop

--- models/user.py
+++ models/user.py
@@ -3,6 +3,8 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+
+
 class User(BaseModel, Base):
     """This is the class for user
     Attributes:
@@ -12,8 +14,11 @@ class User(BaseModel, Base):
         last_name: last name
     """
     __tablename__ = 'users'
+
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
+
     reviews = relationship('Review', cascade='all, delete', backref='user')
+    places = relationship('Place', cascade='all, delete', backref='user')
